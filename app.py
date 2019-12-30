2107fc6f572b268a672ea7594b4b6ac707dd725c
Reem <Roooka219@gmail.com>
2019-12-30 03:01:46 +0200

    Add sudoku_solver_result route and its related necessary modifications.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, redirect, session
 from sudoku import SudokuGame
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = "OlayOlayHoHo"
 
 
 @app.route("/")
@@ -25,8 +26,35 @@ def sudoku_solver():
 
         sudoku = SudokuGame(sudoku_values)
         sudoku.solve_sudoku()
+        sudoku_dict = to_dict(sudoku_values)
+        session['sudoku_dict'] = sudoku_dict
+        return redirect("sudoku_solver_result")
 
-        render_template("sudoku_solver.html", sudoku_values=sudoku_values)
+
+@app.route("/sudoku_solver_result")
+def sudoku_solver_result():
+    sudoku_dict = session['sudoku_dict']
+    return render_template("sudoku_solver_result.html", sudoku_values=sudoku_dict)
 
 
+def to_dict(sudoku_values):
+    sudoku_dict = {
+        'A': None,
+        'B': None,
+        'C': None,
+        'D': None,
+        'E': None,
+        'F': None,
+        'G': None,
+        'H': None,
+        'I': None
+    }
 
+    for i, key in enumerate(sudoku_dict):
+        sudoku_dict[key] = sudoku_values[i*9: i*9+9]
+
+    return sudoku_dict
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
